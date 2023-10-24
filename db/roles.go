5d6f91b845a9fd77c0151cbed1edbfbb4db7fe20
Dominik Tarnowski <dom@tdom.dev>
2023-10-24 21:52:34 +0100

    db: roles

---
+++ db/roles.go
@@ -0,0 +1,47 @@
+package db
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/td0m/doorman"
+)
+
+type Roles struct {
+	pool *pgxpool.Pool
+}
+
+func (r Roles) Add(ctx context.Context, role doorman.Role) error {
+	query := `
+		insert into roles(id, verbs)
+		values($1, $2)
+	`
+
+	if _, err := r.pool.Exec(ctx, query, role.ID, role.Verbs); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (r Roles) Retrieve(ctx context.Context, id string) (*doorman.Role, error) {
+	query := `
+		select verbs
+		from roles
+		where id = $1
+	`
+
+	role := doorman.Role{ID: id}
+
+	err := r.pool.QueryRow(ctx, query, id).Scan(&role.Verbs)
+	if err != nil {
+		return nil, fmt.Errorf("query failed: %w", err)
+	}
+
+	return &role, nil
+}
+
+func NewRoles(pool *pgxpool.Pool) Roles {
+	return Roles{pool}
+}
